e68386ee5bccabe86874c85a3cce7e48f46682f1
Clark Ngo <clarkngo@gmail.com>
2020-03-23 04:53:09 -0700

    removed BrowserAnimationsModule, not compatible with search filter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,17 @@ import { FormsModule } from '@angular/forms';
 
 // search module
 import { Ng2SearchPipeModule } from 'ng2-search-filter';
-import { SearchByNamePipe } from './search-by-name-pipe'
+import { SearchByNamePipe } from './search-by-name-pipe';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 import { FooterComponent } from './footer/footer.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
-
-
-
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
     Ng2SearchPipeModule,
-    BrowserAnimationsModule,
     MaterialModule,
   ],
   declarations: [ AppComponent, SearchByNamePipe, FooterComponent, TopBarComponent ],
