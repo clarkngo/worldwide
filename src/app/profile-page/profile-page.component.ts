5e8764fafa6c095ee7997a44d5a62006db8a2ea8
clarkngo <clarkngo@gmail.com>
2020-05-02 22:35:27 -0700

    small fixes for prod

--- src/app/profile-page/profile-page.component.ts
+++ src/app/profile-page/profile-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { users } from '../users';
 
 @Component({
   selector: 'app-profile-page',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile-page.component.css']
 })
 export class ProfilePageComponent implements OnInit {
+  searchText;
+  users = users;
 
   constructor() { }
 
